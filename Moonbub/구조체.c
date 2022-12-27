0cfc3072d6ba5fb4ee67351b962c0b93e65eaf20
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2022-12-27 14:36:26 +0900

    [문법] 구조체 적용 후

--- Moonbub/구조체.c
+++ Moonbub/구조체.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+struct human {
+	int hp;
+	int speed;
+};
+
 int main(void) 
 {
-	int player_hp = 5;
-	int player_speed = 7;
+	struct human player;
+	player.hp = 5;
+	player.speed = 7;
 
-	int enemy_hp = 2;
-	int enemy_speed = 5;
+	struct human enemy;
+	enemy.hp = 2;
+	enemy.speed = 5;
+
 	printf("플레이어 체력%d 플레이어 속도%d\n", player_hp, player_speed);
 
 	return 0;
