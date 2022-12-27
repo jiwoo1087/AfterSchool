ce32a5dc54eb69e8044f6f2a302199514eaf2ccc
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2022-12-27 10:48:37 +0900

    [문법] 문자열 덧붙이기3

--- Moonbub/문자열 덧붙이기.cpp
+++ Moonbub/문자열 덧붙이기.cpp
@@ -4,17 +4,10 @@
 int main(void)
 {
 	char str[30] = "score : ";
-	//str의 글자수 구하기
-	int str_len = strlen(str);
-
 	char str2[30] = "300";
-	//str2의 글자수 구하기
-	int str2_len = strlen(str2);
 
-	for (int i = 0; i < str2_len; i++)
-		str[i + str_len] = str2[i];
-	str[str_len + str2_len] = '\0';
-	printf("%s", str); 
+	strcat(str, str2);
+	printf("%s", str);
 
 	int score = 300;
 
