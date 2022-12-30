85fff44e73d86a714525faa5f95aad205c416d27
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2022-12-30 11:19:09 +0900

    아이템 효과음 추가

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -44,6 +44,7 @@ struct Item {
 	RectangleShape sprite;
 	int delay;
 	int is_presented;		// 아이템이 떳는지?
+	Sound sound;
 	long presented_time;
 	enum item_type type;
 };
@@ -59,6 +60,7 @@ struct Textures {
 
 struct SButters {
 	SoundBuffer BGM;
+	SoundBuffer item_sound;
 	SoundBuffer rumble;
 };
 
@@ -88,6 +90,7 @@ int main(void)
 	struct SButters sb;
 	sb.BGM.loadFromFile("./resources/sound/BGM.wav");
 	sb.rumble.loadFromFile("./resources/sound/rumble.wav");
+	sb.item_sound.loadFromFile("./resources/sound/item_sound.flac");
 
 	// 윈도창 생성
 	RenderWindow window(VideoMode(W_WIDTH, W_HEIGHT), "AfterSchool");
@@ -175,9 +178,11 @@ int main(void)
 	item[0].sprite.setTexture(&t.item_speed);
 	item[0].delay = 25000;	// 25초
 	item[0].type = SPEED;
+	item[0].sound.setBuffer(sb.item_sound);
 	item[1].sprite.setTexture(&t.item_delay);
 	item[1].delay = 20000;
 	item[1].type = DELAY;
+	item[1].sound.setBuffer(sb.item_sound);
 
 	for (int i = 0; i < ITEM_NUM; i++)
 	{
@@ -355,6 +360,7 @@ int main(void)
 
 			if (item[i].is_presented)
 			{
+				//아이템 획득시 효과를 얻고 사라진다
 				if (is_collide(player.sprite, item[i].sprite))
 				{
 					switch (item[i].type)
@@ -373,6 +379,7 @@ int main(void)
 					}
 					item[i].is_presented = 0;
 					item[i].presented_time = spent_time;
+					item[i].sound.play();
 				}
 			}
 		}
