ae134a16c407e8374e9e901e2f22a43362083c59
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2023-01-02 10:38:32 +0900

    ?로 돌아가기 전에 딜레이 1초

--- AfterSchool2/main.cpp
+++ AfterSchool2/main.cpp
@@ -27,6 +27,10 @@ int main(void)
 	Vector2i mouse_pos;
 	int flipped_num = 0;		// 현재 뒤집혀진 카드의 갯수
 
+	long start_time;
+	long spent_time;
+	long delay_time;			//바로 다시 ?로 뒤집혀지지 않도록 딜레이를 줌
+
 	Texture t[8 + 1];
 	t[0].loadFromFile("./resources/image/ch0.jpg");
 	t[1].loadFromFile("./resources/image/ch1.jpg");
@@ -64,9 +68,13 @@ int main(void)
 		}
 	}
 
+	start_time = clock();
+	delay_time = start_time;
+
 	while (window.isOpen())
 	{
 		mouse_pos = Mouse::getPosition(window);
+		spent_time = clock() - start_time;
 
 		Event event;
 		while (window.pollEvent(event))
@@ -92,6 +100,11 @@ int main(void)
 								{
 									cards[i][j].is_clicked = 1;
 									flipped_num++;
+									//두 개를 뒤집었다면
+									if (flipped_num == 2)
+									{
+										delay_time = spent_time;
+									}
 								}
 							}
 						}
@@ -121,19 +134,27 @@ int main(void)
 		// 뒤집힌 카드가 2개라면 TODO : 두 번째 카다는 바로 다시 뒤집혀지지 않게 하기
 		if (flipped_num == 2)
 		{
-			for (int i = 0; i < S; i++)
+			//카드가 뒤집힌지 2초 이내라면
+			if (spent_time - delay_time <= 1000) 
 			{
-				for (int j = 0; j < S; j++)
-				{
-					cards[i][j].is_clicked = 0;
-				}
 			}
-			flipped_num = 0;
+			else
+			{
+				//다 ?상태로 만들어 버리겠다
+				for (int i = 0; i < S; i++)
+				{
+					for (int j = 0; j < S; j++)
+					{
+						cards[i][j].is_clicked = 0;
+					}
+				}
+				flipped_num = 0;
+			}
 		}
 
 
-		sprintf(info, "(%d, %d ) Click%d\n"
-			, mouse_pos.x, mouse_pos.y, flipped_num);
+		sprintf(info, "(%d, %d ) Click %d %d\n"
+			, mouse_pos.x, mouse_pos.y, spent_time/1000, delay_time/1000);
 		text.setString(info);
 
 		window.clear(Color::Black);
