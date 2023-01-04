72a574814a621bee4b6f87d3bc2ba06076e4872e
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2023-01-04 09:52:19 +0900

    일단 올라가기

--- AfterSchool4/Animation.cpp
+++ AfterSchool4/Animation.cpp
@@ -50,6 +50,13 @@ int main(void)
 			case Event::Closed:
 				window.close();
 				break;
+			case Event::KeyPressed:
+				if (event.key.code == Keyboard::Space)
+				{
+					// มกวม
+					player.sprite.move(0, -3);
+				}
+				break;
 			default:
 				break;
 			}
